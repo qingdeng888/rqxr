05ffb60cdb9a28372666745c1de50a4043dc6d7c
qingdeng888 <35051730+qingdeng888@users.noreply.github.com>
2025-04-15 23:09:01 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-[ -n "${CloudID}" ] && [ -n "${HTTPDZ}" ] && [ -n "${CKEY}" ]
+[ -n "${CloudID}" ] && [ -n "${HTTPDZ}" ] && [ -n "${CKEY}" ] && [ -n "${uuid}" ]
 sed -i "s#CloudID#${CloudID}#g;s#HTTPDZ#${HTTPDZ}#g;s#IDIDID#${CKEY}#g" config.yml
 #sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" /etc/nginx/nginx.conf
 #sed -i "s#RELEASE_RANDOMNESS#${RELEASE_RANDOMNESS}#g" /etc/supervisor/conf.d/supervisord.conf
-uuid=$(head /dev/urandom | tr -dc '0-9' | head -c 12)
+#uuid=$(head /dev/urandom | tr -dc '0-9' | head -c 12)
 sed -i "s/id1212/${uuid}/g" c.yml
 # 伪装 xrayr 执行文件
 RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
